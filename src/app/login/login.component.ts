30502fb013913491de9f58df858060d1610ffc66
Damian <damian.kwiecien@gorrion.pl>
2017-10-15 17:42:35 +0200

    add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services'
+import { Dict } from '../../shared/others'
+import { UserLoginModel } from '../../shared/models'
+import { CookieService } from 'ngx-cookie';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string
+  password: string
+
+  errors: Dict<string> = {}
+
+  constructor(private _authService: AuthService, private _cookieService: CookieService, private _router: Router) { }
+
+  login() {
+    const user = {
+      email: this.email,
+      password: this.password
+    } as UserLoginModel
+
+    this._authService.login(user).subscribe(({ token }) => {
+      this.errors = {}
+      this._cookieService.put('token', token)
+      this._router.navigate(['/'])
+    }, err => {
+      this.errors = err
+    })
+  }
+}
