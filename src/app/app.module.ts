564c4147ceb96a423e9cc4ce8446915364b59ac7
Damian <damian.kwiecien@gorrion.pl>
2017-10-15 17:44:20 +0200

    setup main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { Routes, RouterModule } from '@angular/router'
+import { CookieModule } from 'ngx-cookie'
 
-import { AppComponent } from './app.component';
+import { AuthRouteGuard } from '../shared/route-guards'
+import { AuthService } from '../shared/services'
 
+import { AppComponent } from './app.component'
+import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login/login.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+
+const appRoutes = [{
+  path: '',
+  component: DashboardComponent,
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'dashboard',
+  component: DashboardComponent,
+  canActivate: [AuthRouteGuard]
+}] as Routes
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    CookieModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    AuthRouteGuard,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
