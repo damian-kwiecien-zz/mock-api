4e65ef19cc4628e0c4d8a28e67321072be3b2118
Damian <damian.kwiecien@gorrion.pl>
2017-10-15 17:43:05 +0200

    add register component

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services'
+import { UserRegisterModel } from '../../shared/models'
+import { Dict } from '../../shared/others'
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  email: string
+  password: string
+  confirmPassword: string
+
+  errors: Dict<string> = {}
+
+  constructor(private _authService: AuthService, private _router: Router) { }
+
+  register() {
+    const user = {
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    } as UserRegisterModel
+
+    this._authService.register(user).subscribe(_ => {
+      this.errors = {}
+      this._router.navigate(['/login'])
+    }, err => {
+      this.errors = err
+    })
+  }
+}
