99ebe76b9008ff6324b633a96480da3d8979fd9c
Damian <damian.kwiecien@gorrion.pl>
2017-10-18 20:23:25 +0200

    improve dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { Routes, RouterModule } from '@angular/router'
 import { CookieModule } from 'ngx-cookie'
 
 import { AuthRouteGuard } from '../shared/route-guards'
-import { AuthService } from '../shared/services'
+import { AuthService, MockResponseService, AuthorizedHttpService } from '../shared/services'
+import { MockResponsesResolver } from '../shared/resolvers'
 
 import { AppComponent } from './app.component'
 import { RegisterComponent } from './register/register.component'
 import { LoginComponent } from './login/login.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { TopBarComponent } from './dashboard/top-bar/top-bar.component'
+import { LeftBarComponent } from './dashboard/left-bar/left-bar.component'
+import { SentenceCasePipe } from '../shared/pipes'
+
 
 const appRoutes = [{
   path: '',
   component: DashboardComponent,
+  resolve: { mockResponses: MockResponsesResolver }
 },
 {
   path: 'register',
@@ -35,7 +41,10 @@ const appRoutes = [{
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    TopBarComponent,
+    LeftBarComponent,
+    SentenceCasePipe
   ],
   imports: [
     BrowserModule,
@@ -46,7 +55,10 @@ const appRoutes = [{
   ],
   providers: [
     AuthRouteGuard,
-    AuthService
+    AuthService,
+    AuthorizedHttpService,
+    MockResponseService,
+    MockResponsesResolver
   ],
   bootstrap: [AppComponent]
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,114 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../shared/services'
-
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { AuthService, MockResponseService } from '../../shared/services'
+import { HttpMethod, Dict, HttpMethodEnum } from '../../shared/others'
+import { MockResponse, MockResponseAddModel } from '../../shared/models'
+import { ActivatedRoute } from '@angular/router';
+declare var ace//: AceAjax.Ace
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-  
-  constructor(private _authService: AuthService) { } 
+export class DashboardComponent implements OnInit, AfterViewInit {
+  HttpMethodEnum = HttpMethodEnum
+
+  private _mockResponses: MockResponse[] = []
+
+  set mockResponses(value: MockResponse[]) {
+    this._mockResponses = this.sortMockResponsesAlphabetically(value)
+  }
+
+  get mockResponses(): MockResponse[] {
+    return this._mockResponses
+  }
+  // ||
+  // [{ name: 'aa', body: 'aaa', id: 1, endpoint: 'end', method: 'get' },
+  // { name: 'aAWGa', body: 'aaAWFa', id: 2, endpoint: 'end', method: 'put' },
+  // { name: 'aaAF', body: 'aAWFaa', id: 3, endpoint: 'enAWDd', method: 'get' }]
+
+  mockResponse: MockResponse | MockResponseAddModel = {
+    method: HttpMethodEnum.get
+  } as MockResponseAddModel
+
+  session
+  editor
+  errors: [string, string][] = []
+
+  constructor(private route: ActivatedRoute, private _authService: AuthService, private _mockResponseService: MockResponseService) { }
+
+  ngOnInit() {
+    this.mockResponses = this.route.snapshot.data['mockResponses']
+  }
+
+  ngAfterViewInit() {
+    this.initEditor()
+  }
+
+  private initEditor() {
+    this.editor = ace.edit("editor")
+    this.editor.setTheme("ace/theme/monokai")
+
+    this.session = this.editor.getSession()
+    this.session.setMode("ace/mode/json")
+
+    this.session.on('change', (e) => {
+      this.mockResponse.body = this.session.getValue()
+    })
+  }
+
+  private sortMockResponsesAlphabetically(responses: MockResponse[]) {
+    return responses.sort((a,b) => a.name.localeCompare(b.name))
+  }
+
+  addMockResponse() {
+    this.mockResponse = { method: HttpMethodEnum.get } as MockResponseAddModel
+  }
+
+  selectMockResponse(response: MockResponse) {
+    this.mockResponse = { ...response }
+    this.session.setValue(response.body)
+  }
+
+  saveMockResponse() {
+    if ((this.mockResponse as MockResponse).id)
+      this.updateMockResponse()
+    else
+      this.createMockResponse()
+  }
+
+  private createMockResponse() {
+    const response = this.mockResponse as MockResponseAddModel
+
+    this._mockResponseService.createMockResponse(response)
+      .subscribe(r => {
+        this.mockResponses = [...this.mockResponses, r]
+      }, err => {
+        this.errors = Object.entries(err)
+      })
+  }
+
+  private updateMockResponse() {
+    const response = this.mockResponse as MockResponse
+    const { name, method, body, endpoint } = response
+
+    this._mockResponseService.updateMockResponse(response.id, { name, method, body, endpoint })
+      .subscribe(r => {
+        this.mockResponses = [...this.mockResponses.filter(r2 => r2.id != r.id), r]
+      }, err => {
+        this.errors = Object.entries(err)
+      })
+  }
+
+  deleteMockResponse() {
+    const response = this.mockResponse as MockResponse
+
+    this._mockResponseService.deleteMockResponse(response.id)
+      .subscribe(r => {
+        this.mockResponses = this.mockResponses.filter(r2 => r2.id != r.id)
+        this.addMockResponse()
+      }, err => {
+        this.errors = Object.entries(err)
+      })
+  }
+
 }
