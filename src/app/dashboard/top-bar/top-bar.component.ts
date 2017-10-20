ea5bf5a3ff4852b2f6276aa9c34c5d4bb71326e0
Damian <damian.kwiecien@gorrion.pl>
2017-10-20 18:43:18 +0200

    improve top bar component

--- src/app/dashboard/top-bar/top-bar.component.ts
+++ src/app/dashboard/top-bar/top-bar.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, Output, EventEmitter } from '@angular/core'
 
 @Component({
   selector: 'top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent { }
+export class TopBarComponent { 
+  @Output() readonly export: EventEmitter<void> = new EventEmitter<void>()
+
+  emitExportEvent() {
+    this.export.emit()
+  }
+
+}
